98ccd425416d2b6fcbb7b92b3601b8ceb6a424f6
NomadRebel <nikhilverma4209@gmail.com>
2025-04-18 14:22:42 +0530

    project completed maybe later update with css.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rock-Paper-Scissors
+This is Rock, Paper, Scissors game. It will run till one player scores five points. After this the page will reload and new game will begun.
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,65 +1,179 @@
-console.log('Hello World');
-// function that generates random values
+// first let's declare the random number function then I'll call the function when button is clicked
+
 function getComputerChoice(){
-  let a = Math.floor(Math.random() * 3);
-  if (a===0){
+  let randomNumber = Math.floor(Math.random()*3)
+  if (randomNumber === 0){
     return "rock";
   }
-  else if (a===1){
+  else if (randomNumber === 1){
     return "paper";
   }
-  else if (a===2){
+  else {
     return "scissors";
-  }  
+  }
+}
+// declaring score variables
+let humanScore = 0;
+let computerScore = 0;
+// adding functions for specific buttons
+
+
+// playRock will execute when rock button is clicked
+function playRock(){
+  let a = getComputerChoice();
+  let b = "rock";
+  let c;
+  console.log(a);
+  console.log(b);
+  if(a===b){
+    c =  "draw";
+  }
+  else if (b === "rock" && a === "scissors"){
+    c = "win";
+  }
+  else{
+    c = "lose";
+  }
+  return c;
+};
+
+
+// playPaper will execute when paper button is clicked
+function playPaper(){
+  let a = getComputerChoice();
+  let b = "paper";
+  let c;
+  console.log(a);
+  console.log(b);
+  if(a===b){
+    c =  "draw";
+  }
+  else if (b === "paper" && a === "rock"){
+    c = "win";
+  }
+  else{
+    c = "lose";
+  }
+  return c;
+};
+
+
+// playScissors will execute when scissors button is clicked
+function playScissors(){
+  let a = getComputerChoice();
+  let b = "scissors";
+  let c;
+  console.log(a);
+  console.log(b);
+  if(a===b){
+    c =  "draw";
+  }
+  else if (b === "scissors" && a === "paper"){
+    c = "win";
+  }
+  else{
+    c = "lose";
+  }
+  return c;
 }
 
-  //function to take user input
-  function getHumanChoice(){
-    return (prompt("rock,paper,scissors?", '')).toLowerCase();
-  }
-  //score variable
-    let humanScore = 0;
-    let computerScore = 0;
+// selecting div id = one from html
+const div = document.getElementById("one");
 
-    //function that compares user's input and computer choice to give the desired result
-    function playRound(humanChoice, computerChoice){
-      if(humanChoice === computerChoice){
-        humanScore++;
-        return "draw";
-      }
-      else if (humanChoice === "paper" && computerChoice==="rock"){
-        humanScore++;
-        return "win";
-      }
-      else if (humanChoice === "rock" && computerChoice==="scissors"){
-        humanScore++;
-        return "win"
-      }
+// creating new div with variable name score that will contain the score of the game
+const score = document.createElement("div");
 
-      else if (humanChoice === "scissors" && computerChoice==="paper"){
-        return "Tie";
-      }
-      else{
-        computerScore++;
-        return "lose";
-      }
-    }
-function playGame(){
-  for(let round = 0; round <5; round++){
-      let humanSelection = getHumanChoice();
-      console.log(humanSelection);
-      let computerSelection = getComputerChoice();
-      console.log(computerSelection);
-      playRound(humanSelection, computerSelection);
+// paragraph that will go inside score and display the scores
+const pOne = document.createElement("p");
+const pTwo = document.createElement("p");
+
+
+// function for rock button when it is clicked
+const rock = document.querySelector("#rock");
+rock.addEventListener("click",function(){
+  let a = playRock(); 
+  if(a==="win"){  // giving condition so it wil match the return value of playRock and increase the humanScore by 1.
+    humanScore++;
   }
-  if (humanScore>computerScore) {
-    console.log("you win");
+  else if(a==="draw"){
+     humanScore;
+     computerScore;
   }
   else {
-    console.log("you lose");
+     computerScore++;
   }
-  console.log("humanScore "+humanScore);
-  console.log("computerScore "+computerScore);
-}
-  let playedGame = playGame();
-  console.log(playedGame);
+  // checking condition if any of the players scores 5 then it will display the message and reload the page
+  if(humanScore===5){
+    alert("you won");
+    location.reload();
+  }
+  else if(computerScore===5){
+    alert("you lost");
+    location.reload();
+  }
+
+  // this will create new element in html and add the score variable 
+  pOne.textContent = "Your Score: " + humanScore + "/5";
+  pTwo.textContent = "Computer Score: " +computerScore + "/5";
+  score.appendChild(pOne);
+  score.appendChild(pTwo);
+  div.appendChild(score);
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click",function(){
+  let a = playPaper();
+  if(a==="win"){
+    humanScore++;
+  }
+  else if(a==="draw"){
+     humanScore;
+     computerScore;
+  }
+  else {
+     computerScore++;
+  }
+  if(humanScore===5){
+    alert("you won");
+    location.reload();
+  }
+  else if(computerScore===5){
+    alert("you lost");
+    location.reload();
+  }
+  pOne.textContent = "Your Score: " + humanScore + "/5";
+  pTwo.textContent = "Computer Score: " +computerScore + "/5";
+  score.appendChild(pOne);
+  score.appendChild(pTwo);
+  div.appendChild(score);
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click",function(){
+  let a = playScissors();
+  if(a==="win"){
+    humanScore++;
+  }
+  else if(a==="draw"){
+     humanScore;
+     computerScore;
+  }
+  else {
+     computerScore++;
+    }
+  
+  if(humanScore===5){
+    alert("you won");
+    location.reload();
+  }
+  else if(computerScore===5){
+    alert("you lost");
+    location.reload();
+  }
+
+  pOne.textContent = "Your Score: " + humanScore +"/5";
+  pTwo.textContent = "Computer Score: " + computerScore + "/5";
+  score.appendChild(pOne);
+  score.appendChild(pTwo);
+  div.appendChild(score);
+});
